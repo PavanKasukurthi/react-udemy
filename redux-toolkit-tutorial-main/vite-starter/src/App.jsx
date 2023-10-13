2ecf2b5653e37d8dcb6f3f9ed03b81cde63876b0
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-10-13 19:10:15 +0530

    createAsyncThunk

--- redux-toolkit-tutorial-main/vite-starter/src/App.jsx
+++ redux-toolkit-tutorial-main/vite-starter/src/App.jsx
@@ -2,20 +2,35 @@ import Navbar from './components/Navbar'
 import CartContainer from './components/CartContainer'
 
 import { useDispatch, useSelector } from 'react-redux'
-import { calculateTotal } from './features/cart/cartSlice'
+import { calculateTotal, getCartItems } from './features/cart/cartSlice'
 import { useEffect } from 'react'
 import cartItems from './cartItems'
+import Modal from './components/Modal'
 
 function App() {
-  const { cartItems } = useSelector((state) => state.cart)
+  const { cartItems, isLoading } = useSelector((state) => state.cart)
+  const { isOpen } = useSelector((state) => state.modal)
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(calculateTotal())
   }, [cartItems])
 
+  useEffect(() => {
+    dispatch(getCartItems())
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
